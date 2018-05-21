f254f7c9cdb898b7a766bdc84abe420c469d1d2f
taohonghui <347785474@qq.com>
2018-05-21 08:20:20 +0800

    提交初始工程目录

---
+++ harmmer/src/components/MainView.vue
@@ -0,0 +1,66 @@
+<template>
+<el-container>
+  <el-header><myHeader></myHeader></el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+   <el-container>
+      <el-footer>Footer</el-footer>
+  </el-container>
+
+</el-container>
+</template>
+<script>
+import myHeader from './Header.vue'
+export default {
+  name: 'MainView',
+  data () {
+    return {}
+  },
+  components: {
+    myHeader
+  }
+}
+</script>
+
+<style>
+.el-header{
+  padding: 0px;
+}
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
